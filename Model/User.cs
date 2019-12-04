149d3e22412f6f04eb30824465dc9f00be3dfbb0
Andreea Vasiliu <andr77c6@edu.easj.dk>
2019-12-04 13:19:52 +0100

    Adds ViewModel, Model, View files. Adds a new User class. (EditQuantity to be implemented)

---
+++ Model/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Park_and_Garden.Model
+{
+    public class User
+    {
+        public User(string name, string username, string password)
+        {
+            Name = name;
+            Username = username;
+            Password= password;
+        }
+
+        public string Password { get; }
+
+        public string Username { get; }
+
+        public string Name { get; }
+
+        public void EditQuantity()
+        { }
+    }
+}
